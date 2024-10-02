99a543b0481e07b41504c0d6b5aa94ab27f1c432
simonpcouch <simonpatrickcouch@gmail.com>
2024-10-02 15:44:30 -0500

    don't supply startup message when missing .env

--- R/zzz.R
+++ R/zzz.R
@@ -2,19 +2,9 @@
 
 .onLoad <- function(libname, pkgname) {
   # automatically source .env for API keys on package load
-  rlang::try_fetch(
-    dotenv::load_dot_env(),
-    error = function(e) {
-      rlang::inform(
-        c(
-          ".env file not found.",
-          "i" = "See {.fn cli_pal} for more information."
-        ),
-        class = "packageStartupMessage"
-      )
-    }
-  )
-
+  if (file.exists(".env")) {
+    load_dot_env()
+  }
 
   rlang::env_bind(
     rlang::ns_env("clipal"),
